c759f05bbaa540bff4217470d5b5735d6b485269
Mena <000817498@mohawkcollege.ca>
2022-09-16 11:04:14 -0400

    Actually finished the assignment now

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,13 @@ void setup() {
   Serial.begin(115200);
 
   Serial.println("\n\nHello, World!");
+  Serial.println("Michael Mena 000817498");
+  Serial.println("ESP8266 Chip ID: "+ (String)ESP.getChipId());
+  Serial.println("Flash Chip ID: "  + (String)ESP.getFlashChipId());
 }
 
 void loop() {
+  delay(2000);
   // put your main code here, to run repeatedly:
+  Serial.println("Milliseconds since d1 was started: "+ String(millis()) );
 }
